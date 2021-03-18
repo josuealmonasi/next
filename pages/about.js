380f1fdcfedd4b41d5053454f3845c34538694d2
Josue Almonasi <josue.almonasi@hotmail.com>
2021-03-18 12:15:14 -0600

    Added routes and separated some css into modules

---
+++ pages/about.js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+import React from 'react';
+
+const about = () => {
+  return (
+    <div>
+      <Head>
+        <title>About</title>
+      </Head>
+      Abouts
+    </div>
+  );
+};
+
+export default about;
